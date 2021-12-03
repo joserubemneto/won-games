42cb7a2768400a9f8dcfbada5c8abebeaed5a450
Jose Rubem <joserubemn31@gmail.com>
2021-12-03 17:03:30 +0000

    update Banner with ribbon

--- src/components/Banner/test.tsx
+++ src/components/Banner/test.tsx
@@ -28,4 +28,26 @@ describe('<Banner />', () => {
       'https://test-image.com'
     )
   })
+
+  it('should render a Ribbon', () => {
+    renderWithTheme(
+      <Banner
+        {...props}
+        ribbon="20% OFF"
+        ribbonSize="small"
+        ribbonColor="secondary"
+      />
+    )
+
+    const ribbon = screen.getByText(/20% off/i)
+
+    expect(ribbon).toBeInTheDocument()
+    expect(ribbon).toHaveStyle({
+      backgroundColor: '#3CD3C1'
+    })
+    expect(ribbon).toHaveStyle({
+      height: '2.6rem',
+      fontSize: '1.2rem'
+    })
+  })
 })
