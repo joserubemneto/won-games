96d37f63cc889c42248f230bf48ee32f06023e28
root <joserubemn31@gmail.com>
2021-12-15 21:39:48 +0000

    feat: add Base component

---
+++ src/templates/Base/index.tsx
@@ -0,0 +1,25 @@
+import * as S from './styles'
+import Menu from 'components/Menu'
+import { Container } from 'components/Container'
+import Footer from 'components/Footer'
+
+export type BaseProps = {
+  children: React.ReactNode
+}
+
+const Base = ({ children }: BaseProps) => (
+  <section>
+    <Container>
+      <Menu />
+    </Container>
+    {children}
+
+    <S.SectionFooter>
+      <Container>
+        <Footer />
+      </Container>
+    </S.SectionFooter>
+  </section>
+)
+
+export default Base
