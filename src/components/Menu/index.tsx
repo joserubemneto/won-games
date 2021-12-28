692d56025410c5dcb8d93011f1c4abaf61d840fb
Rubem Neto <joserubemn31@gmail.com>
2021-12-28 23:47:23 +0000

    feat: update Menu component

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,15 +1,17 @@
-import * as S from './styles'
-
-import Logo from 'components/Logo'
-import { useState } from 'react'
-import Button from 'components/Button'
-import MediaMatch from 'components/MediaMatch'
 import Link from 'next/link'
 
-import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outlined/ShoppingCart'
+import { useState } from 'react'
+import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2'
 import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
 import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
-import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2'
+
+import Button from 'components/Button'
+import Logo from 'components/Logo'
+import MediaMatch from 'components/MediaMatch'
+import * as S from './styles'
+import CartDropdown from 'components/CartDropdown'
+import CartIcon from 'components/CartIcon'
+import UserDropdown from 'components/UserDropdown'
 
 export type MenuProps = {
   username?: string
@@ -37,9 +39,11 @@ const Menu = ({ username }: MenuProps) => {
       <MediaMatch greaterThan="medium">
         <S.MenuNav>
           <Link href="/" passHref>
-            <S.MenuLink href="#">Home</S.MenuLink>
+            <S.MenuLink>Home</S.MenuLink>
           </Link>
-          <S.MenuLink href="#">Explore</S.MenuLink>
+          <Link href="/games" passHref>
+            <S.MenuLink>Explore</S.MenuLink>
+          </Link>
         </S.MenuNav>
       </MediaMatch>
 
@@ -48,31 +52,46 @@ const Menu = ({ username }: MenuProps) => {
           <SearchIcon aria-label="Search" />
         </S.IconWrapper>
         <S.IconWrapper>
-          <ShoppingCartIcon aria-label="Open Shopping Cart" />
+          <MediaMatch greaterThan="medium">
+            <CartDropdown />
+          </MediaMatch>
+          <MediaMatch lessThan="medium">
+            <Link href="/cart">
+              <a>
+                <CartIcon />
+              </a>
+            </Link>
+          </MediaMatch>
         </S.IconWrapper>
-
         <MediaMatch greaterThan="medium">
-          {!username && (
+          {!username ? (
             <Link href="/sign-in" passHref>
               <Button as="a">Sign in</Button>
             </Link>
+          ) : (
+            <UserDropdown username={username} />
           )}
         </MediaMatch>
       </S.MenuGroup>
 
       <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
         <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
-
         <S.MenuNav>
           <Link href="/" passHref>
-            <S.MenuLink href="#">Home</S.MenuLink>
+            <S.MenuLink>Home</S.MenuLink>
           </Link>
-          <S.MenuLink href="#">Explore</S.MenuLink>
+          <Link href="/games" passHref>
+            <S.MenuLink>Explore</S.MenuLink>
+          </Link>
 
           {!!username && (
             <>
-              <S.MenuLink href="#">My account</S.MenuLink>
-              <S.MenuLink href="#">Wishlist</S.MenuLink>
+              <Link href="/profile/me" passHref>
+                <S.MenuLink>My profile</S.MenuLink>
+              </Link>
+              <Link href="/profile/wishlist" passHref>
+                <S.MenuLink>Wishlist</S.MenuLink>
+              </Link>
             </>
           )}
         </S.MenuNav>
@@ -80,11 +99,13 @@ const Menu = ({ username }: MenuProps) => {
         {!username && (
           <S.RegisterBox>
             <Link href="/sign-in" passHref>
-              <Button as="a">Log in now</Button>
+              <Button fullWidth size="large" as="a">
+                Sign in
+              </Button>
             </Link>
             <span>or</span>
             <Link href="/sign-up" passHref>
-              <S.CreateAccount title="Sign in">Sign Up</S.CreateAccount>
+              <S.CreateAccount title="Sign Up">Sign Up</S.CreateAccount>
             </Link>
           </S.RegisterBox>
         )}
