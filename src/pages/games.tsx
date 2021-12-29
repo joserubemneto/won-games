737401cc541d5a3020299b068c643dfbb35120ad
Rubem Neto <joserubemn31@gmail.com>
2021-12-29 16:24:51 +0000

    feat: add games graphql queries

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,15 +1,40 @@
 import GamesTemplate, { GamesTemplateProps } from 'templates/Games'
 import filterItemsMock from 'components/ExploreSidebar/mock'
-import gamesMock from 'components/GameCardSlider/mock'
+import { initializeApollo } from 'utils/apollo'
+import { QUERY_GAMES } from 'graphql/queries/games'
+import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames'
 
 export default function GamesPage(props: GamesTemplateProps) {
   return <GamesTemplate {...props} />
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query<QueryGames, QueryGamesVariables>({
+    query: QUERY_GAMES,
+    variables: {
+      limit: 9
+    }
+  })
+
   return {
     props: {
-      games: gamesMock,
+      revalidate: 60,
+      games: data.games.map((game) => ({
+        title: game.name,
+        developer: game.developers[0].name,
+        img: game.cover
+          ? `http://localhost:1337/${game.cover?.url}`
+          : 'img/auth-bg.jpg',
+        price:
+          game.price > 0
+            ? new Intl.NumberFormat('en', {
+                style: 'currency',
+                currency: 'USD'
+              }).format(game.price)
+            : 'FREE'
+      })),
       filterItems: filterItemsMock
     }
   }
