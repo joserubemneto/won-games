92796f7f85186b7084ea10d4555d7ddada9ff3fd
Rubem Neto <joserubemn31@gmail.com>
2022-03-02 18:30:12 +0000

    feat: update games page to use gamesMapper

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -3,6 +3,7 @@ import filterItemsMock from 'components/ExploreSidebar/mock'
 import { initializeApollo } from 'utils/apollo'
 import { QUERY_GAMES } from 'graphql/queries/games'
 import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames'
+import { gamesMapper } from 'utils/mappers'
 
 export default function GamesPage(props: GamesTemplateProps) {
   return <GamesTemplate {...props} />
@@ -11,7 +12,9 @@ export default function GamesPage(props: GamesTemplateProps) {
 export async function getStaticProps() {
   const apolloClient = initializeApollo()
 
-  const { data } = await apolloClient.query<QueryGames, QueryGamesVariables>({
+  const {
+    data: { games }
+  } = await apolloClient.query<QueryGames, QueryGamesVariables>({
     query: QUERY_GAMES,
     variables: {
       limit: 9
@@ -21,15 +24,7 @@ export async function getStaticProps() {
   return {
     props: {
       revalidate: 60,
-      games: data.games.map((game) => ({
-        slug: game.slug,
-        title: game.name,
-        developer: game.developers[0].name,
-        img: game.cover
-          ? `http://localhost:1337/${game.cover?.url}`
-          : 'img/auth-bg.jpg',
-        price: game.price
-      })),
+      games: gamesMapper(games),
       filterItems: filterItemsMock
     }
   }
